e37bcf03e5849955e8e419d9aff89b7dcb095270
Igor Klepuszewski <igor.klepuszewski@gmail.com>
2022-02-06 09:20:19 +0100

    premigration car model

---
+++ portal/car/models.py
@@ -0,0 +1,37 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+# Create your models here.
+
+class CarMain(models.Model):
+    make = models.CharField(max_length=16)
+    model = models.CharField(max_length=32)
+
+class CarDetail(models.Model):
+    CAR_COLORS = [
+        ("RED", "Red"),
+        ("BLU", "Blue"),
+        ("YEL", "Yellow"),
+        ("BLA", "Black"),
+        ("WHI", "White")
+    ]
+    CAR_FUELS = [
+        ("DIE", "Diesel"),
+        ("PET", "Petrol"),
+        ("HYB", "Hybrid"),
+        ("LPG", "LPG"),
+        ("ELE", "Electric"),
+    ]
+    production_date = models.DateField()
+    color = models.CharField(max_length=3, choices=CAR_COLORS, default="BLA")
+    seats = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(9)])
+    fuel = models.CharField(max_length=3, choices=CAR_FUELS, default="PET")
+    power = models.PositiveIntegerField()
+
+class Car(models.Model):
+    main = models.ForeignKey(CarMain, on_delete=models.CASCADE)
+    detail = models.OneToOneField(CarDetail, on_delete=models.PROTECT)
+
+    def delete(self):
+        super(Car, self).delete()
+        self.detail.delete()
+
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'car',
+    'website'
 ]
 
 ROOT_URLCONF = 'portal.urls'
